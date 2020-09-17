1e6ddf82d50011bd823a666242d722b38a24dce7
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-17 14:57:14 +0300

    in-memory db configuration

--- Ecommerce.Api.Products/Startup.cs
+++ Ecommerce.Api.Products/Startup.cs
@@ -1,5 +1,7 @@
+using Ecommerce.Api.Products.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +20,7 @@ namespace Ecommerce.Api.Products
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ProductsDbContext>(options => options.UseInMemoryDatabase("Products"));
             services.AddControllers();
         }
 
